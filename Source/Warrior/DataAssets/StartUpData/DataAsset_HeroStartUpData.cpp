736b2893ec18c1993ec6b7702adcc6649bcd089f
crapa36 <crapa36@gmail.com>
2025-10-29 21:06:34 +0900

    2

--- Source/Warrior/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ Source/Warrior/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -1,5 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "DataAssets/StartUpData/DataAsset_HeroStartUpData.h"
+#include "AbilitySystem/Abilitys/WarriorGameplayAbility.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
 
+bool FWarriorHeroAbilitySet::IsValid() const {
+    return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel) {
+    Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+    for (const FWarriorHeroAbilitySet& AbilitySet : HeroStartUpAbilitySet) {
+        if (!AbilitySet.IsValid()) continue;
+        FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+        AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+        AbilitySpec.Level = ApplyLevel;
+        AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+
+        InASCToGive->GiveAbility(AbilitySpec);
+    }
+}
